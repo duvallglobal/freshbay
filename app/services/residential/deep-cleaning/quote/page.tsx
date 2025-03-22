e816cf8e7bd851716efbd9ac0570088bcca559bb
mjduvall <mjduvall@duvallglobal.com>
2025-03-22 06:18:46 -0400

    edo

--- app/services/residential/deep-cleaning/quote/page.tsx
+++ app/services/residential/deep-cleaning/quote/page.tsx
@@ -345,7 +345,6 @@ export default function DeepCleaningQuotePage() {
                               </PopoverTrigger>
                               <PopoverContent className="w-auto p-0" align="start">
                                 <Calendar
-                                  mode="single"
                                   selected={field.value}
                                   onSelect={field.onChange}
                                   disabled={(date) => date < new Date()}
